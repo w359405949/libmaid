f77fb40d942e380698226bd669fa7464e6b1b1fa
w359405949 <w359405949@gmail.com>
2014-10-22 13:53:01 +0800

    update gitignore net/build_example.sh

--- net/build_example.sh
+++ net/build_example.sh
@@ -1,3 +1,3 @@
-mono ProtoGen/bin/Debug/protogen.exe -i:example/hello.proto -o:example/Hello.cs
-mcs -t:library example/Hello.cs -reference:example/protobuf-net.dll
-mono precompile/bin/Debug/precompile.exe example/Hello.dll -t:maid.example.HelloSerializer -o:example/HelloSerializer.dll
+mono ProtoGen/bin/Debug/protogen.exe -i:maid/example/proto/hello.proto -o:maid/example/proto/Hello.cs
+mcs -t:library maid/example/proto/Hello.cs -reference:maid/example/proto/protobuf-net.dll
+mono precompile/bin/Debug/precompile.exe maid/example/proto/Hello.dll -t:maid.example.HelloSerializer -o:maid/example/proto/HelloSerializer.dll
