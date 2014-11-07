69d54e5624b6aa0ac0d5ec9dcbae307f93daebdb
w359405949 <w359405949@gmail.com>
2014-11-07 15:42:51 +0800

    clean

--- build_proto.sh
+++ build_proto.sh
@@ -10,7 +10,8 @@ mv proto/*.cc src/
 mv proto/*.py python/maid/
 echo "done"
 echo
-echo "building proto for net"
-mono net/ProtoGen/bin/Debug/protogen.exe -i:proto/controller.proto -o:net/maid/maid/library/ControllerMeta.cs
-mcs -t:library net/maid/maid/library/ControllerMeta.cs -reference:net/maid/maid/library/protobuf-net.dll -sdk:2
-mono net/precompile/bin/Debug/precompile.exe net/maid/maid/library/ControllerMeta.dll -t:maid.proto.ControllerMetaSerializer -o:net/maid/maid/library/ControllerMetaSerializer.dll
+
+#echo "building proto for net"
+#mono net/ProtoGen/bin/Debug/protogen.exe -i:proto/controller.proto -o:net/maid/maid/library/ControllerMeta.cs
+#mcs -t:library net/maid/maid/library/ControllerMeta.cs -reference:net/maid/maid/library/protobuf-net.dll -sdk:2
+#mono net/precompile/bin/Debug/precompile.exe net/maid/maid/library/ControllerMeta.dll -t:maid.proto.ControllerMetaSerializer -o:net/maid/maid/library/ControllerMetaSerializer.dll
