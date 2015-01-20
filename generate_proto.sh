529627d5a23118e78ad2f9ecb9f2cdb1a5ff2929
w359405949 <w359405949@gmail.com>
2015-01-20 19:17:20 +0800

    1, clean file 2, add python generate

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 pushd include
 echo "generate proto for cpp"
-protoc --cpp_out=. --proto_path=. --proto_path=$HOME/.local/include maid/*.proto
+protoc --cpp_out=. --python_out=. --proto_path=. --proto_path=$HOME/.local/include maid/*.proto
 popd
 
 mv include/maid/*.pb.cc src/
-#mv proto/*.py python/maid/
+mv include/maid/*.py python/maid/
 echo "done"
