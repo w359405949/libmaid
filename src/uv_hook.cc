da76b746c631828b1724a6b3d30eda659ead4483
w359405949 <w359405949@gmail.com>
2015-02-10 00:35:46 +0800

    add uv hook

--- src/uv_hook.cc
+++ src/uv_hook.cc
@@ -1,3 +1,25 @@
+#include <stdlib.h>
+#include "glog/logging.h"
 #include "uv.h"
 
-UV_EXTERN uv_loop_t* uv_default_loop(void);
+uv_once_t once_t;
+uv_key_t loop_key;
+
+void init_once()
+{
+    uv_key_create(&loop_key);
+}
+
+UV_EXTERN uv_loop_t* uv_default_loop(void)
+{
+    uv_once(&once_t, init_once);
+
+    uv_loop_t* loop = (uv_loop_t*)uv_key_get(&loop_key);
+    if (NULL == loop) {
+        loop = (uv_loop_t*)malloc(sizeof(uv_loop_t));
+        uv_loop_init(loop);
+        uv_key_set(&loop_key, loop);
+    }
+    DLOG(INFO) << "loop:" << (int64_t)loop;
+    return loop;
+}
